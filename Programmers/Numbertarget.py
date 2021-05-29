8c4d4d5448bdfc6b514da90e59ed4890ac5dd422
Next Page 01 <pilotymc@gmail.com>
2021-05-29 18:31:01 +0900

    Update Numbertarget.py

--- Programmers/Numbertarget.py
+++ Programmers/Numbertarget.py
@@ -10,3 +10,23 @@ def solution(numbers, target):
         return solution(numbers[1:], target+numbers[0]) + solution(numbers[1:], target-numbers[0])
 
     
+#my similar idea code
+
+def solution(numbers, target):
+    num_len = len(numbers)
+    res = 0
+    def dfs(idx,cnt):
+        if idx == num_len:
+            if target == cnt:
+                nonlocal res #use nonlocal to have outside stuff
+                res += 1
+                return
+            else:
+                return
+        else:
+            dfs(idx+1,cnt+numbers[idx]) # summon this until idx is numlen and go plus1 if target is same
+            dfs(idx+1,cnt-numbers[idx])
+
+
+    dfs(0,0)
+    return res
